10bf4d561d2515fca705115e04e357a2c5ddabae
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-05-01 14:22:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,13 +4,14 @@ const charCounts = new Map();
 for (const char of str) {
 charCounts.set(char, (charCounts.get(char) || 0) + 1);
 }
+ 
 	
 for (const char of str) {
 if (charCounts.get(char) === 1) {
 return char;
 }
 }
-return null;
+return str;  
 }
 const userInput = prompt("Enter a string:")
 const result = firstNonRepeatedChar(userInput);
