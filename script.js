b42c87cfe4cf97e3149bbe5cf31830d79eef2f5b
Vivek Tiwari <102945885+Vivek-Tiwari20@users.noreply.github.com>
2023-05-01 14:30:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
 function firstNonRepeatedChar(str) {
-const charCounts = new Map();
-for (const char of str) {
-charCounts.set(char, (charCounts.get(char) || 0) + 1);
-}
+  const charCounts = new Map();
+  for (const char of str) {
+  charCounts.set(char, (charCounts.get(char) || 0) + 1);
+  }
+    
+  for (const char of str) {
+  if (charCounts.get(char) === 1) {
+  return char;
+  }
+  }
+  return "There's no non-repeated character in the string.";
+  }
   
-for (const char of str) {
-if (charCounts.get(char) === 1) {
-return char;
-}
-}
-return "There's no non-repeated character in the string.";
-}
-
-const userInput = prompt("Enter a string:");
-const result = firstNonRepeatedChar(userInput);
-if (result) {
-alert(The first non-repeated character is "${result}");
-} else {
-alert("There's no non-repeated character in the string.");
-}
+  const userInput = prompt("Enter a string:");
+  const result = firstNonRepeatedChar(userInput);
+  if (result) {
+  alert(`The first non-repeated character is "${result}"`);
+  } else {
+  alert("There's no non-repeated character in the string.");
+  }
